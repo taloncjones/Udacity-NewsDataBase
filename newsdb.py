1d2643fbdf1c8ff9fd2969354200c92eaf9b91ae
Talon Jones <taloncjones@gmail.com>
2019-02-09 23:04:34 +0000

    Add psql queries to top_art and top_auth

--- newsdb.py
+++ newsdb.py
@@ -1,30 +1,42 @@
 # "Database code" for the news DB.
 
 import psycopg2
-import bleach
 
 DB_name = "news"
 
 def top_art():
-  """Return top three news articles of all time."""
-#  db = psycopg2.connect(database=DB_name)
-#  c = db.cursor()
-#  c.execute("select content, time from posts order by time desc;")
-#  posts = c.fetchall()
-#  db.close()
-  return top
+  # Return top three news articles of all time.
+  db = psycopg2.connect(database=DB_name)
+  c = db.cursor()
+  c.execute("select articles.slug, count(*) as num
+    from articles
+      join log on log.path like '%' || articles.slug || '%'
+    group by articles.slug
+    order by num desc
+    limit 3;")
+  topthree = c.fetchall()
+  db.close()
+  return topthree
 
 def top_auth():
-  """Return top authors of all time."""
-#  db = psycopg2.connect(database=DB_name)
-#  c = db.cursor()
-#  c.execute("select content, time from posts order by time desc;")
-#  posts = c.fetchall()
-#  db.close()
-  return top
+  # Return top authors of all time.
+  db = psycopg2.connect(database=DB_name)
+  c = db.cursor()
+  c.execute("select authors.name, count(*) as num
+    from articles
+      join log on log.path like '%' || articles.slug || '%'
+      join authors on articles.author = authors.id
+    group by authors.name
+    order by num desc;")
+  topauth = c.fetchall()
+  db.close()
+  return topauth
 
 def high_error():
-  """Return days where >1% of requests had errors."""
+  # Return days where >1% of requests had errors.
+
+
+
 #  db = psycopg2.connect(database=DB_name)
 #  c = db.cursor()
 #  c.execute("select content, time from posts order by time desc;")
